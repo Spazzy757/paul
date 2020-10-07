8124e2063ac1752135b642d20606871b4e413089
Spazzy <brendankamp757@gmail.com>
2020-10-07 07:48:58 +0200

    Added functionality to comment on a PR

--- pkg/router/router.go
+++ pkg/router/router.go
@@ -1,18 +1,24 @@
 package router
 
 import (
+	"github.com/Spazzy757/paul/pkg/github"
 	"github.com/gorilla/mux"
-	"log"
+	"io/ioutil"
 	"net/http"
 )
 
 func GetRouter() *mux.Router {
 	r := mux.NewRouter()
-	r.HandleFunc("/payload", GithubHandler)
+	r.HandleFunc("/", GithubWebHookHandler)
 	return r
 }
 
-func GithubHandler(w http.ResponseWriter, r *http.Request) {
-	log.Printf("%v", r.Body)
+func GithubWebHookHandler(w http.ResponseWriter, r *http.Request) {
+	jsonByte, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
+	defer r.Body.Close()
+	github.IncomingWebhook(jsonByte, r)
 	w.WriteHeader(http.StatusOK)
 }

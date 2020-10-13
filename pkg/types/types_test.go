b23a92f0de6e7fa2e87c18d3eb795aad5b2d737b
Spazzy <brendankamp757@gmail.com>
2020-10-13 15:17:54 +0200

    Improved Types Test to test if failure occurs

--- pkg/types/types_test.go
+++ pkg/types/types_test.go
@@ -1,9 +1,14 @@
 package types
 
 import (
-	"github.com/stretchr/testify/assert"
+	"bytes"
 	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestLoadConfig(t *testing.T) {
@@ -25,4 +30,23 @@ func TestLoadConfig(t *testing.T) {
 		assert.NotEqual(t, paulConfig.PullRequests.DogsEnabled, false)
 	})
 
+}
+
+func TestLoadConfigFails(t *testing.T) {
+	// Only run actual test in subprocess
+	if os.Getenv("SUB_PROCESS") == "1" {
+		var paulConfig PaulConfig
+		var buf bytes.Buffer
+		log.SetOutput(&buf)
+		paulConfig.LoadConfig([]byte(`% ^ & HHH`))
+		return
+	}
+	cmd := exec.Command(os.Args[0], "-test.run=TestLoadConfigFails")
+	cmd.Env = append(os.Environ(), "SUB_PROCESS=1")
+	err := cmd.Run()
+	// Cast the error as *exec.ExitError and compare the result
+	e, ok := err.(*exec.ExitError)
+	expectedErrorString := "exit status 1"
+	assert.Equal(t, true, ok)
+	assert.Equal(t, expectedErrorString, e.Error())
 }

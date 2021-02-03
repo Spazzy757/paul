e4361a6d422704cfa6c006a0f9fdb754d093c20b
Spazzy <brendankamp757@gmail.com>
2021-02-03 15:06:58 +0100

    Added a check for the PAUL.yaml in the .github directory

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"testing"
 
@@ -52,5 +53,63 @@ func TestGetPaulConfig(t *testing.T) {
 		assert.NotEqual(t, "", cfg.PullRequests.OpenMessage)
 		assert.NotEqual(t, cfg.PullRequests.CatsEnabled, false)
 		assert.NotEqual(t, cfg.PullRequests.DogsEnabled, false)
+	})
+	t.Run("Test Read Paul Config Returns Valid Paul Config if its in .github directory", func(t *testing.T) {
+		yamlFile, err := ioutil.ReadFile("../../PAUL.yaml")
+		assert.Equal(t, nil, err)
+
+		mClient, mux, serverURL, teardown := test.GetMockClient()
+		defer teardown()
+		mux.HandleFunc(
+			"/repos/Spazzy757/paul/contents/.github",
+			func(w http.ResponseWriter, r *http.Request) {
+				assert.Equal(t, r.Method, "GET")
+				fmt.Fprint(w, `[{
+		            "type": "dir",
+		            "name": ".github",
+		            "path": ".github"
+		           },{
+		            "type": "file",
+		            "name": "PAUL.yaml",
+		            "download_url": "`+serverURL+baseURLPath+`/download/PAUL.yaml"
+		        }]`)
+			},
+		)
+		mux.HandleFunc("/download/PAUL.yaml", func(w http.ResponseWriter, r *http.Request) {
+			assert.Equal(t, r.Method, "GET")
+			log.Println("here")
+
+			fmt.Fprint(w, string(yamlFile))
+		})
+
+		owner := "Spazzy757"
+		repo := "paul"
+		cfg, err := GetPaulConfig(
+			context.Background(),
+			owner, repo,
+			"main",
+			mClient,
+		)
+		assert.Equal(t, nil, err)
+		assert.NotEqual(t, "", cfg.PullRequests.OpenMessage)
+		assert.NotEqual(t, cfg.PullRequests.CatsEnabled, false)
+		assert.NotEqual(t, cfg.PullRequests.DogsEnabled, false)
+	})
+	t.Run("Test Read Paul Error Returns an Empty Config", func(t *testing.T) {
+		mClient, _, _, teardown := test.GetMockClient()
+		defer teardown()
+
+		owner := "Spazzy757"
+		repo := "paul"
+		cfg, err := GetPaulConfig(
+			context.Background(),
+			owner, repo,
+			"main",
+			mClient,
+		)
+		assert.NotEqual(t, nil, err)
+		assert.Equal(t, "", cfg.PullRequests.OpenMessage)
+		assert.Equal(t, cfg.PullRequests.CatsEnabled, false)
+		assert.Equal(t, cfg.PullRequests.DogsEnabled, false)
 	})
 }
